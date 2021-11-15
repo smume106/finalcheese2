7b05049defcea8ebf7314c8a4960456f626a3745
Maggie Karczewski <maggie.karczewski@gmail.com>
2021-11-15 18:51:41 -0500

    music and final level update
    music will now continue from scene to scene. final level will switch to next scene after button on fridge is clicked.

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -8,6 +8,7 @@ public class MainMenu : MonoBehaviour
     public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
     }
 
     public void Replay()
