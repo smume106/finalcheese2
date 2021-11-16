bf73741465fd870b2a02770a1fae7607066acb83
Kyle0323 <eenigenburgk@gmail.com>
2021-11-16 03:38:01 -0500

    First Interact Push
    With this step, the code for  interact has been completely written.

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Interact : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private DisplayImage currentDisplay;
+
     void Start()
     {
-        
+        currentDisplay = GameObject.Find("displayImage").GetComponent<DisplayImage>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector2 rayPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(rayPosition, Vector2.zero, 100);
+
+            if(hit && hit.transform.tag == "Interactable")
+            {
+                hit.transform.GetComponent<IInteractable>().Interact(currentDisplay);
+            }
+        }
     }
 }
