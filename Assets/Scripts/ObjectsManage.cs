81fdb3cdabd2c739333fe77e6b46f2fe5521d557
Kyle0323 <eenigenburgk@gmail.com>
2021-12-06 23:07:38 -0500

    Object Manager Implementation

--- Assets/Scripts/ObjectsManage.cs
+++ Assets/Scripts/ObjectsManage.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class ObjectsManage : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private DisplayImage currentDisplay;
+
+    public GameObject[] ObjectsToManage;
+
     void Start()
     {
-        
+        currentDisplay = GameObject.Find("dipslayImage").GetComponent<DisplayImage>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        ManageObjects();
+    }
+    void ManageObjects()
+    {
+        for (int i = 0; i < ObjectsToManage.Length; i++)
+        {
+            if (ObjectsToManage[i].name == currentDisplay.GetComponent<SpriteRenderer>().sprite.name)
+            {
+                ObjectsToManage[i].SetActive(true);
+            }
+            else
+            {
+                ObjectsToManage[i].SetActive(false);
+            }
+        }
     }
 }
