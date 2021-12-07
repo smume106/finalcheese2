6ed130c2098f99b4e6242bbf11d3eb901be25ea6
Kyle0323 <eenigenburgk@gmail.com>
2021-12-07 03:31:23 -0500

    Forgot to save a script oops

--- Assets/Scripts/SolveLock.cs
+++ Assets/Scripts/SolveLock.cs
@@ -12,7 +12,8 @@ public class SolveLock : MonoBehaviour, IInteractable
         if (transform.parent.GetComponent<NumberLock>().currentIndividualIndex[transform.GetSiblingIndex()] > 9)
             transform.parent.GetComponent<NumberLock>().currentIndividualIndex[transform.GetSiblingIndex()] = 0;
 
-        this.gameObject.GetComponent<SpriteRenderer>().sprite = transform.parent.GetComponent<NumberLock>().numberSprites[transform.parent.GetComponent<NumberLock>().currentIndividualIndex[transform.GetSiblingIndex()]];
+        this.gameObject.GetComponent<SpriteRenderer>().sprite =
+            transform.parent.GetComponent<NumberLock>().numberSprites[transform.parent.GetComponent<NumberLock>().currentIndividualIndex[transform.GetSiblingIndex()]];
 
 
     }
