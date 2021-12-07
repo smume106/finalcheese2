6766f260be761ed6cb711e78a4d26076cdd315f4
Kyle0323 <eenigenburgk@gmail.com>
2021-12-07 03:43:26 -0500

    Lock Code may work now...

--- Assets/Scripts/NumberLock.cs
+++ Assets/Scripts/NumberLock.cs
@@ -10,13 +10,46 @@ public class NumberLock : MonoBehaviour
 
     public int[] currentIndividualIndex = { 0, 0, 0 };
 
+    private bool isOpen;
+
     void Start()
     {
+        isOpen = false;
         LoadAllNumberSprites();  
+    }
+
+    void Update()
+    {
+        OpenLock();
     }
 
     void LoadAllNumberSprites()
     {
         numberSprites = Resources.LoadAll<Sprite>("Sprites/numbers");
+    }
+
+    bool VerifyCorrectCode()
+    {
+        bool correct = true;
+
+        for(int i = 0; i < 3; i++)
+        {
+            if(Password[i] != transform.GetChild(i).GetComponent<SpriteRenderer>().sprite.name.Substring(transform.GetChild(i).GetComponent<SpriteRenderer>().sprite.name.Length - 1)[0])
+            {
+                correct = false;
+            }
+        }
+        return correct;
+    }
+
+    void OpenLock()
+    {
+        if (isOpen) return;
+
+        if(VerifyCorrectCode())
+        {
+            isOpen = true;
+            Debug.Log("Password correct! You are awesome!");
+        }
     }
 }
